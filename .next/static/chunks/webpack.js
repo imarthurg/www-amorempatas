5a23a82c9990abb9f97b706329a5c4ba8cf8acf5
Arthur Lima <arthur.lima@zrp.com.br>
2021-03-06 21:00:29 -0300

    fix: navlinks margin for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "074401e39e1802acc0ca";
+/******/ 	var hotCurrentHash = "f976ea3aae766899abba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
