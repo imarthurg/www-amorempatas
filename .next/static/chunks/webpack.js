23bcaaa3edb46d336ee1fe9e2148f24f8793972f
Arthur Lima <arthur.lima@zrp.com.br>
2021-03-07 03:45:02 -0300

    feat: adds animal list filters

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "72ca89bb094767642b81";
+/******/ 	var hotCurrentHash = "2c066d60c961caba84ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/AnimalList/index.js
+++ components/AnimalList/index.js
@@ -5,6 +5,7 @@ import AnimalCard from '../AnimalCard';
 import Button from '../../components/Button';
 
 import DogImage from '../../assets/images/dog.jpg';
+import CatImage from '../../assets/images/cat.jpg';
 import styles from './styles.module.css';
 
 const animals = [
@@ -32,11 +33,41 @@ const animals = [
     type: 'dog',
     imgSrc: DogImage,
   },
+
+  {
+    id: 5,
+    name: 'Nilce',
+    type: 'cat',
+    imgSrc: CatImage,
+  },
+
+  ,
+  {
+    id: 6,
+    name: 'Nilce',
+    type: 'cat',
+    imgSrc: CatImage,
+  },
+
+  {
+    id: 7,
+    name: 'Nilce',
+    type: 'cat',
+    imgSrc: CatImage,
+  },
+
+  {
+    id: 8,
+    name: 'Nilce',
+    type: 'cat',
+    imgSrc: CatImage,
+  },
 ];
 
 const categories = [
-  { label: 'Cachorros', emoji: ':dog:' },
-  { label: 'Gatos', emoji: ':cat:' },
+  { label: 'Todos', value: '', emoji: '' },
+  { label: 'Cachorros', value: 'dog', emoji: ':dog:' },
+  { label: 'Gatos', value: 'cat', emoji: ':cat:' },
 ];
 
 export default function AnimalList() {
@@ -48,22 +79,24 @@ export default function AnimalList() {
   return (
     <div className={styles.container}>
       <div className={styles.categories_container}>
-        {categories.map(({ label, emoji }) => (
+        {categories.map(({ label, emoji, value }) => (
           <Button
-            key={label}
+            key={value}
             className={styles.categories_container_button}
-            status={selectedCategory === label && 'primary'}
-            handleClick={() => selectCategory(label)}
+            status={selectedCategory === value && 'primary'}
+            handleClick={() => selectCategory(value)}
           >
             {label}&nbsp;
-            {emojify(emoji)}
+            {emoji && emojify(emoji)}
           </Button>
         ))}
       </div>
 
-      {animals.map(({ name, imgSrc }) => (
-        <AnimalCard key={name} name={name} imgSrc={imgSrc} />
-      ))}
+      {animals
+        .filter(({ type }) => !selectedCategory || type === selectedCategory)
+        .map(({ id, name, imgSrc }) => (
+          <AnimalCard key={id} name={name} imgSrc={imgSrc} />
+        ))}
     </div>
   );
 }
