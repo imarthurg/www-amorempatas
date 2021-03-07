09f21c2233daa98507064e01ebbab2dace46081a
Arthur Lima <arthur.lima@zrp.com.br>
2021-03-06 21:47:51 -0300

    feat: optimizations in the main bg images

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f976ea3aae766899abba";
+/******/ 	var hotCurrentHash = "35ff56595591e82c2785";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
