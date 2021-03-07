8468adaea578c4a36aa1ab4db4ebc47871ba3fba
Arthur Lima <arthur.lima@zrp.com.br>
2021-03-07 03:56:58 -0300

    fix: navbar buttons disposition for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2c066d60c961caba84ef";
+/******/ 	var hotCurrentHash = "e48628ad12c43a03a50d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
