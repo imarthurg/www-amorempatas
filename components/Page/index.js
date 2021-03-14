05118d09206b60a79ce65c55b834f374acf58df1
Cleberton Junior <cleberton.juninho@gmail.com>
2021-03-13 21:12:15 -0300

    changed name class 'off' to 'offset'

--- components/Page/index.js
+++ components/Page/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 export default function Page({ children }) {
   return <div className={styles.container}>
     <Header />
-    <div class='off'></div>
+    <div class='offset'></div>
     {children}
   </div>;
 }
